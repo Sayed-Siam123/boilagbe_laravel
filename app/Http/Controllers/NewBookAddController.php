50044e44c701981eb32f78a29fa5a3c70d6451c3
Sayed-Siam123 <Msiam18@gmail.com>
2018-11-04 05:21:45 +0600

    pushed on 4-11-18

--- app/Http/Controllers/NewBookAddController.php
+++ app/Http/Controllers/NewBookAddController.php
@@ -130,40 +130,6 @@ class NewBookAddController extends Controller
             echo "nai";
         }
 
-
-        /*if ($request ->login == "submit"){
-
-            echo Session::get('username');
-            $username = Session::get('username');
-
-            $this->validate($request,['image_name' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
-            ]);
-
-           if ($request->hasFile('image_name')){
-
-               echo "pic paisi";
-               $image = $request -> file('image_name');
-               $name = $username. '.' .$image->getClientOriginalExtension();
-               $image_resize = Image::make($image->getRealPath());
-               $image_resize->resize(300,300);
-               $destination = public_path('/book');
-               $image_resize->save($destination.'/'.$name);
-
-
-
-           }
-
-           else{
-               echo "pic paini";
-           }
-
-
-        }
-
-        else{
-            echo "paini";
-        }*/
-
     }
 
 
